7e4efdae4a8a7b2e308191e81b95085cd55bcb8f
danekshea <danekshea@gmail.com>
2022-09-24 00:11:58 +1000

    updated .env.example

--- src/examples - WIP/end-to-end-mint.ts
+++ src/examples - WIP/end-to-end-mint.ts
@@ -15,7 +15,7 @@ async function main(ownerPrivateKey:string, network:string) {
     //Deploy the contract with the below parameters. 5m gas limit and 60gwei gas price seems to work fine for the NFT contracts. 
     //Make sure you have enough sandbox ETH on this address. Check out https://imxfaucet.xyz/ to get some.
     //TODO: add error handling for the user not having enough funds in wallet
-    const deployedContract = await deployContract(ownerPrivateKey, 'Asset', 'Contract Name', 'SYMBOL', network, '5000000', '60000000000');
+    const deployedContract = await deployContract(ownerPrivateKey, 'Asset', 'Contract Name', 'SYMBOL', network, '5000000', '2000000000');
     console.log('Deployed contract address: ' + deployedContract.address)
     console.log('Now we wait 3 minutes while the contract deploys...')
 
